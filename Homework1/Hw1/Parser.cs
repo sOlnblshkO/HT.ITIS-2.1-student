d1c56c68683d0c97290d2a64b337c5f9ff38642a
daaammirka <cdbacjdorz@yandex.com>
2023-09-08 21:52:37 +0300

    Сделана первая домашка

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,13 +7,39 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        AppropriationValuesWithValidateCountArguments(args, out val1, out operation, out val2);
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "/" => CalculatorOperation.Divide,
+            "*" => CalculatorOperation.Multiply,
+            _ => throw new InvalidOperationException()
+        };
+    }
+    
+    private static double ParseStringToDouble(string arg)
+    {
+        if (double.TryParse(arg, out var number))
+            return number;
+        throw new ArgumentException();
+    }
+    
+    private static void AppropriationValuesWithValidateCountArguments(
+        string[] args,
+        out double val1,
+        out CalculatorOperation operation,
+        out double val2)
+    {
+        if (!IsArgLengthSupported(args)) throw new ArgumentException();
+        val1 = ParseStringToDouble(args[0]);
+        operation = ParseOperation(args[1]);
+        val2 = ParseStringToDouble(args[2]);
     }
 }
