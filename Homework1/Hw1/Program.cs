dc5ec78c0ac60cd3c6b40a394870fde9af817c42
Almaz Shamilov <pepegapeepoclown4head@gmail.com>
2023-09-08 18:50:18 +0300

    тестовый коммит

--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -1,7 +1,9 @@
-﻿var arg1 = args[0];
+﻿using Hw1;
+
+var arg1 = args[0];
 var operation = args[1];
 var arg2 = args[2];
 
 // TODO: implement calculator logic
-var result = arg1 + operation + arg2;
+var result = Calculator.Calculate(double.Parse(arg1), CalculatorOperation.Plus, double.Parse(arg2));
 Console.WriteLine(result);
