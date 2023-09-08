90ff5debbcc7645f636fd843a770456df6081f12
Almaz Shamilov <pepegapeepoclown4head@gmail.com>
2023-09-08 20:39:58 +0300

    выполнена первая домашняя работа

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -2,18 +2,39 @@
 
 public static class Parser
 {
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
+    public static void ParseCalcArguments(string[] args,
+        out double val1,
+        out CalculatorOperation operation,
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args))
+        {
+            throw new ArgumentException("Неправильное количество аргументов");
+        }
+
+        if (!(double.TryParse(args[0], out val1) && double.TryParse(args[2], out val2)))
+        {
+            throw new ArgumentException("Даны не числа");
+        }
+
+        operation = ParseOperation(args[1]);
+        if (operation == CalculatorOperation.Undefined)
+        {
+            throw new InvalidOperationException("Дана неправильная операция");
+        }
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => CalculatorOperation.Undefined
+        };
     }
 }
--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -1,9 +1,5 @@
 ﻿using Hw1;
 
-var arg1 = args[0];
-var operation = args[1];
-var arg2 = args[2];
-
-// TODO: implement calculator logic
-var result = Calculator.Calculate(double.Parse(arg1), CalculatorOperation.Plus, double.Parse(arg2));
+Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+var result = Calculator.Calculate(val1, operation, val2);
 Console.WriteLine(result);
