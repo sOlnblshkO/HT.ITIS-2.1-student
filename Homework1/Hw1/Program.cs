2445cb156dc3281012b31c908defc1363938eacd
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2023-09-09 23:14:59 +0300

    попытка №2 workflow

--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -2,6 +2,6 @@
 var operation = args[1];
 var arg2 = args[2];
 
-// TODO: implement calculator logic
+// TODO: implement calculator logicc
 var result = arg1 + operation + arg2;
 Console.WriteLine(result);
