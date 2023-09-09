94ca5b0980f7ef2e096807e576f4cd91fbacd69d
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2023-09-09 14:46:48 +0300

    Fixed parser and CLI calculator

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -2,18 +2,33 @@
 
 public static class Parser
 {
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
-        out double val2)
+    public static void ParseCalcArguments(
+        string[] args,
+        out double val1,
+        out CalculatorOperation operation,
+        out double val2
+    )
     {
-        throw new NotImplementedException();
+        if (!(IsArgLengthSupported(args)))
+            throw new ArgumentException("Too many arguments");
+        if (!Double.TryParse(args[0], out val1))
+            throw new ArgumentException("First argument is not a real number");
+        operation = ParseOperation(args[1]);
+        if (operation == CalculatorOperation.Undefined)
+            throw new InvalidOperationException("Undefined operation");
+        if (!Double.TryParse(args[2], out val2))
+            throw new ArgumentException("Third argument is not a real number");
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
-    private static CalculatorOperation ParseOperation(string arg)
-    {
-        throw new NotImplementedException();
-    }
+    private static CalculatorOperation ParseOperation(string arg) =>
+        arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => CalculatorOperation.Undefined
+        };
 }
--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -1,7 +1,5 @@
-﻿var arg1 = args[0];
-var operation = args[1];
-var arg2 = args[2];
+﻿using Hw1;
 
-// TODO: implement calculator logic
-var result = arg1 + operation + arg2;
-Console.WriteLine(result);
+Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+
+Console.WriteLine(Calculator.Calculate(val1, operation, val2));
