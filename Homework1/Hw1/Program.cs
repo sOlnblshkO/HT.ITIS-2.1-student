9558b662715043203ad181d74e7cbc1808b16035
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2023-09-09 17:47:59 +0300

    Wrapped in a try-catch block

--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -1,5 +1,11 @@
 ﻿using Hw1;
 
-Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
-
-Console.WriteLine(Calculator.Calculate(val1, operation, val2));
+try
+{
+    Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+    Console.WriteLine(Calculator.Calculate(val1, operation, val2));
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
