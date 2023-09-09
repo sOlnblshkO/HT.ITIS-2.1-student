fa76a7a6114ce99359fc338c010cc512feb909a3
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2023-09-09 23:18:12 +0300

    попытка №3 ...

--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -2,6 +2,6 @@
 var operation = args[1];
 var arg2 = args[2];
 
-// TODO: implement calculator logicc
+// TODO: implement calculator logic
 var result = arg1 + operation + arg2;
 Console.WriteLine(result);
