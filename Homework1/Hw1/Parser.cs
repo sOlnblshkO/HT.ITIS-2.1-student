ac2856866d92b71b0b938a5fa69a179ec0218a36
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-09-10 19:18:36 +0300

    init

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -14,6 +14,6 @@ public static class Parser
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        throw new NotImplementedException(); 
     }
 }
