4bf4134afa562c5e1a78e61408bad5952453e7a5
Liana <a>
2023-09-10 14:48:07 +0300

    Revert "homework2.1.1 написан калькулятор и парсер"
    This reverts commit 2da74eae87e20a3586e15ac229e95a7fc8e1c4b7.

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,50 +7,13 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        if (IsArgLengthSupported(args))
-        {
-            if (double.TryParse(args[0], out val1))
-            {
-                Convert.ToDouble(args[0]);
-            }
-            else
-            {
-                throw new ArgumentException();
-            }
-
-            operation = ParseOperation(args[1]);
-
-            if (double.TryParse(args[2], out val2))
-            {
-                Convert.ToDouble(args[2]);
-            }
-            else
-            {
-                throw new ArgumentException();
-            }
-
-            if (!(double.TryParse(args[0], out val1) && double.TryParse(args[2], out val1)))
-            {
-                throw new ArgumentException();
-            }
-        }
-        else
-        {
-            throw new ArgumentException();
-        }
+        throw new NotImplementedException();
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        return arg switch
-        {
-            "+" => CalculatorOperation.Plus,
-            "-" =>  CalculatorOperation.Minus,
-            "*" =>  CalculatorOperation.Multiply,
-            "/" =>  CalculatorOperation.Divide,
-            _ => throw new InvalidOperationException()
-        };
+        throw new NotImplementedException();
     }
 }
