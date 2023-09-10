945f86e72bb198b00854148e6697ef93d9adaaaf
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2023-09-10 13:27:14 +0300

    тест правильного мерджа

--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -1,9 +1,7 @@
 ﻿using Hw1;
 
-
 Parser.ParseCalcArguments(args,
     out var val1,
     out var calculatorOperation,
     out var val2);
-
-Console.WriteLine(Calculator.Calculate(val1, calculatorOperation,val2));
+Console.WriteLine(Calculator.Calculate(val1,calculatorOperation,val2));
