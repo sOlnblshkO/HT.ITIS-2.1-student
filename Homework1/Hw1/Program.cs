ad9388f1dbc57e49337feb424444e32368a0ef79
Ruzan Valeev <ruzan.valeeff@yandex.ru>
2023-09-10 14:19:32 +0300

    Complete first homework

--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -1,7 +1,10 @@
-﻿var arg1 = args[0];
+﻿using Hw1;
+
+var arg1 = args[0];
 var operation = args[1];
 var arg2 = args[2];
 
-// TODO: implement calculator logic
-var result = arg1 + operation + arg2;
+var arguments = new string[] {arg1, operation, arg2};
+Parser.ParseCalcArguments(arguments, out double val1, out CalculatorOperation op, out double val2);
+var result = Calculator.Calculate(val1, op, val2);
 Console.WriteLine(result);
