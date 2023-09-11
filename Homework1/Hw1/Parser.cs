7c2481a3320052d98ec90ba901adef538571b108
ilklmkn <klimkin_ilya2004@mail.ru>
2023-09-11 07:02:04 +0300

    update hw1

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,14 +7,21 @@ public static class Parser
         out CalculatorOperation operation,
         out double val2)
     {
-        if (!double.TryParse(args[0], out val1) || !double.TryParse(args[2], out val2) ||
+        if (!IsArgTypeSupported(args[0], args[2], out val1, out val2) ||
             !IsArgLengthSupported(args))
         {
-            throw new ArgumentException();
+            throw new ArgumentException("Inappropriate array format");
         }
 
         operation = ParseOperation(args[1]);
     }
+
+    private static bool IsArgTypeSupported(string arg1, string arg2, out double val1, out double val2)
+    {
+        val2 = 0;
+        return double.TryParse(arg1, out val1) && double.TryParse(arg2, out val2);
+    }
+
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
@@ -26,7 +33,7 @@ public static class Parser
             "-" => CalculatorOperation.Minus,
             "*" => CalculatorOperation.Multiply,
             "/" => CalculatorOperation.Divide,
-            _ => throw new InvalidOperationException()
+            _ => throw new InvalidOperationException("Unavailable operation")
         };
     }
 }
