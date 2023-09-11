357044f2e0e2e32999972e78722638e2536772c0
Akhmed Mirsaitov <bignumbergames@gmail.com>
2023-09-11 10:55:13 +0300

    HW-1 Завершающий коммит

--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -4,4 +4,4 @@ Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
 
 var result = Calculator.Calculate(val1, operation, val2);
 Console.WriteLine(result);
-Console.ReadLine();
+Console.ReadKey();
