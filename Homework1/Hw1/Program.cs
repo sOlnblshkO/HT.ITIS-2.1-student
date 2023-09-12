7bc2679d7500f98631381e1187a80aea89ac07a9
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2023-09-13 00:00:36 +0300

    добавлен отлов исключений в program.cs

--- Homework1/Hw1/Program.cs
+++ Homework1/Hw1/Program.cs
@@ -3,7 +3,18 @@
 while (true)
 {
     var input = Console.ReadLine()?.Split(' ');
-    Parser.ParseCalcArguments(input, out var val1, out var operation, out var val2);
-    var result = Calculator.Calculate(val1, operation, val2);
-    Console.WriteLine(result);
+    try
+    {
+        Parser.ParseCalcArguments(input, out var val1, out var operation, out var val2);
+        var result = Calculator.Calculate(val1, operation, val2);
+        Console.WriteLine(result);
+    }
+    catch (ArgumentException exception)
+    {
+        Console.WriteLine(exception.Message);
+    }
+    catch (InvalidOperationException exception)
+    {
+        Console.WriteLine(exception.Message);
+    }
 }
