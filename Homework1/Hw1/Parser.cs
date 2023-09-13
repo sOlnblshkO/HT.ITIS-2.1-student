d37102bf9d97c5a95c8aec74c9ba79e73b1ccd29
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-09-13 23:08:36 +0300

    Added parser

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -2,18 +2,43 @@
 
 public static class Parser
 {
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
+    public static void ParseCalcArguments(string[] args,
+        out double val1,
+        out CalculatorOperation operation,
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args))
+        {
+            throw new ArgumentException("There must be 3 arguments!");
+        }
+
+        val1 = ParseArgument(args[0]);
+        val2 = ParseArgument(args[2]);
+        operation = ParseOperation(args[1]);
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException(); 
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => throw new InvalidOperationException("Incorrect input operation")
+        };
+    }
+
+    private static double ParseArgument(string arg)
+    {
+
+        if (Double.TryParse(arg, out var number))
+        {
+            return number;
+        }
+
+        throw new ArgumentException("Incorrect input number");
     }
 }
