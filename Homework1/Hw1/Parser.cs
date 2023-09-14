69312ae23d28b7d2377d27ae4e38526951ce5917
Resqdeeo <112940499+Resqdeeo@users.noreply.github.com>
2023-09-14 22:17:36 +0300

    Revert "Added parser of arguments"

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,34 +7,13 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        if (!IsArgLengthSupported(args))
-            throw new ArgumentException();
-        val1 = ParseArgument(args[0]);
-        val2 = ParseArgument(args[2]);
-        operation = ParseOperation(args[1]);
-    }
-    private static double ParseArgument(string arg)
-    {
-
-        if (Double.TryParse(arg, out var number))
-        {
-            return number;
-        }
-
-        throw new ArgumentException("Incorrect input number");
+        throw new NotImplementedException();
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        switch (arg)
-        {
-            case "+": return CalculatorOperation.Plus;
-            case "-": return CalculatorOperation.Minus;
-            case "*": return CalculatorOperation.Multiply;
-            case "/": return CalculatorOperation.Divide;
-            default: throw new InvalidOperationException();
-        }
+        throw new NotImplementedException();
     }
 }
