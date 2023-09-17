eea4715faa06ebc366024336c3541e01507d8716
Almaz Shamilov <pepegapeepoclown4head@gmail.com>
2023-09-17 23:37:24 +0300

    выполнена вторая домашняя работа

--- Tests.CSharp/Homework2/ParserTests.cs
+++ Tests.CSharp/Homework2/ParserTests.cs
@@ -12,7 +12,17 @@ public class ParserTests
     [InlineData("/", CalculatorOperation.Divide)]
     public void TestCorrectOperations(string operation, CalculatorOperation operationExpected)
     {
-        throw new NotImplementedException();
+        // arrange 
+        var args = new[] { "228", operation, "322" };
+        
+        // act
+        Parser.ParseCalcArguments(args, out var val1, out var actualOperation, out var val2);
+        
+        // assert
+        Assert.Equal(228, val1);
+        Assert.Equal(operationExpected, actualOperation);
+        Assert.Equal(322, val2);
+        
     }
 
     [HomeworkTheory(Homeworks.HomeWork2)]
@@ -21,18 +31,33 @@ public class ParserTests
     [InlineData("a", "+", "f")]
     public void TestParserWrongValues(string val1, string operation, string val2)
     {
-        throw new NotImplementedException();
+        // arrange
+        var args = new[] { val1, operation, val2 };
+        
+        // assert
+        Assert.Throws<ArgumentException>(
+            () => Parser.ParseCalcArguments(args, out _, out _, out _));
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestParserWrongOperation()
     {
-        throw new NotImplementedException();
+        // arrange
+        var args = new[] { "1488", "=", "1337" };
+        
+        // assert
+        Assert.Throws<InvalidOperationException>(
+            () => Parser.ParseCalcArguments(args, out _, out _, out _));
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestParserWrongLength()
     {
-        throw new NotImplementedException();
+        // arrange
+        var args = new[] { "1.23", "-", "9.43", "-8.5353" };
+        
+        // assert
+        Assert.Throws<ArgumentException>(
+            () => Parser.ParseCalcArguments(args, out _, out _, out _));
     }
 }
