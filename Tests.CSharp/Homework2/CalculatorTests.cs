7f2be86eccc6b8cede51c0aacb70d1d57c500eea
BuLbl4 <113981565+BuLbl4@users.noreply.github.com>
2023-09-20 15:27:40 +0300

    hw2 done

--- Tests.CSharp/Homework2/CalculatorTests.cs
+++ Tests.CSharp/Homework2/CalculatorTests.cs
@@ -12,37 +12,38 @@ public class CalculatorTests
     [InlineData(15, 5, CalculatorOperation.Divide, 3)]
     public void TestAllOperations(int value1, int value2, CalculatorOperation operation, int expectedValue)
     {
-        Assert.Equal(Calculator.Calculate(value1, operation, value2), expectedValue);
+        var operationResult = Calculator.Calculate(value1, operation, value2);
+        
+        Assert.Equal(expectedValue, operationResult);
     }
 
-    
     [Homework(Homeworks.HomeWork2)]
     public void TestInvalidOperation()
     {
-        Assert.Throws<ArgumentOutOfRangeException>(() => Calculator.Calculate(0, CalculatorOperation.Undefined, 10));
+        Assert.Throws<ArgumentOutOfRangeException>(() => Calculator.Calculate(13, CalculatorOperation.Undefined, 24));
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestDividingNonZeroByZero()
     {
-        var actual = Calculator.Calculate(0, CalculatorOperation.Divide, 10);
-
-        Assert.Equal(0, actual);
+        var result = Calculator.Calculate(10, CalculatorOperation.Divide, 0);
+        
+        Assert.Equal(double.PositiveInfinity, result);
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestDividingZeroByNonZero()
     {
-        var actual = Calculator.Calculate(10, CalculatorOperation.Divide, 0);
-
-        Assert.Equal(double.PositiveInfinity, actual);
+        var result = Calculator.Calculate(0, CalculatorOperation.Divide, 10);
+        
+        Assert.Equal(0, result);
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestDividingZeroByZero()
     {
-        var actual = Calculator.Calculate(0, CalculatorOperation.Divide, 0);
-
-        Assert.Equal(double.NaN, actual);
+        var result = Calculator.Calculate(0, CalculatorOperation.Divide, 0);
+        
+        Assert.Equal(double.NaN, result);
     }
 }
--- Tests.CSharp/Homework2/ParserTests.cs
+++ Tests.CSharp/Homework2/ParserTests.cs
@@ -12,20 +12,19 @@ public class ParserTests
     [InlineData("/", CalculatorOperation.Divide)]
     public void TestCorrectOperations(string operation, CalculatorOperation operationExpected)
     {
-        var args = new[] { "15", operation, "5" };
-        
+        var args = new[] { "7", operation, "13" };
         
         Parser.ParseCalcArguments(args, out var val1, out var operationResult, out var val2);
-
-        Assert.Equal(15, val1);
-        Assert.Equal(operationExpected, operationResult);
-        Assert.Equal(5, val2);
+        
+        Assert.Equal(7, val1);
+        Assert.Equal(operationResult, operationExpected);
+        Assert.Equal(13, val2);
     }
-    
+
     [HomeworkTheory(Homeworks.HomeWork2)]
     [InlineData("f", "+", "3")]
     [InlineData("3", "+", "f")]
-    [InlineData("c", "+", "s")]
+    [InlineData("r", "+", "r")]
     public void TestParserWrongValues(string val1, string operation, string val2)
     {
         var args = new[] { val1, operation, val2 };
@@ -36,7 +35,7 @@ public class ParserTests
     [Homework(Homeworks.HomeWork2)]
     public void TestParserWrongOperation()
     {
-        var args = new[] { "3", "and", "4" };
+        var args = new[] { "15", "nice", "16" };
 
         Assert.Throws<InvalidOperationException>(() => Parser.ParseCalcArguments(args, out _, out _, out _));
     }
@@ -44,7 +43,7 @@ public class ParserTests
     [Homework(Homeworks.HomeWork2)]
     public void TestParserWrongLength()
     {
-        var args = new[] { "c", ".", "4", "5", "2" };
+        var args = new[] { "14", "+", "16", "26" };
 
         Assert.Throws<ArgumentException>(() => Parser.ParseCalcArguments(args, out _, out _, out _));
     }
