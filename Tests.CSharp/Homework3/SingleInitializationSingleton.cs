a6812b97138bf892c7c4c913f6d67468a7086d6a
daaammirka <cdbacjdorz@yandex.com>
2023-09-24 16:29:10 +0300

    Сделана 3 домашка, реализован потокобезопасный синглетон с помощью Lazy

--- Tests.CSharp/Homework3/SingleInitializationSingleton.cs
+++ Tests.CSharp/Homework3/SingleInitializationSingleton.cs
@@ -5,6 +5,9 @@ public class SingleInitializationSingleton
     public const int DefaultDelay = 3_000;
     private static readonly object Locker = new();
 
+    private static Lazy<SingleInitializationSingleton> _lazy =
+        new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton(DefaultDelay), true);
+
     private static volatile bool _isInitialized = false;
 
     private SingleInitializationSingleton(int delay = DefaultDelay)
@@ -16,15 +19,18 @@ public class SingleInitializationSingleton
 
     public int Delay { get; }
 
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
+    public static SingleInitializationSingleton Instance => _lazy.Value;
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        _lazy = new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton(DefaultDelay),true);
+        _isInitialized = false;
     }
 
     public static void Initialize(int delay)
     {
-        throw new NotImplementedException();
+        if (_isInitialized) throw new InvalidOperationException("Cannot be initialized more than once!");
+        _lazy = new Lazy<SingleInitializationSingleton>(() => new SingleInitializationSingleton(delay),true);
+        _isInitialized = true;
     }
 }
