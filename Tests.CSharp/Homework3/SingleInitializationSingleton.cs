8082fde648880bc378777d71664d882fbe4f8345
irekkkarimov <irekkkarimov@mail.ru>
2023-09-25 16:06:21 +0300

    HomeWork 3 done

--- Tests.CSharp/Homework3/SingleInitializationSingleton.cs
+++ Tests.CSharp/Homework3/SingleInitializationSingleton.cs
@@ -4,7 +4,7 @@ public class SingleInitializationSingleton
 {
     public const int DefaultDelay = 3_000;
     private static readonly object Locker = new();
-
+    private static SingleInitializationSingleton _instance;
     private static volatile bool _isInitialized = false;
 
     private SingleInitializationSingleton(int delay = DefaultDelay)
@@ -16,15 +16,53 @@ public class SingleInitializationSingleton
 
     public int Delay { get; }
 
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
+    public static SingleInitializationSingleton Instance
+    {
+        
+        get
+        {
+            if (_isInitialized)
+            {
+                Thread.Sleep(_instance.Delay);
+                return _instance;
+            }
+            _instance = new SingleInitializationSingleton();
+            _isInitialized = true;
+            Thread.Sleep(_instance.Delay);
+            return _instance;
+        }
+    }
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        _instance = null;
+        _isInitialized = false;
     }
 
     public static void Initialize(int delay)
     {
-        throw new NotImplementedException();
+        try
+        {
+            if (!_isInitialized)
+            {
+                lock (Locker)
+                {
+                    if (!_isInitialized)
+                    {
+                        _instance = new SingleInitializationSingleton(delay);
+                        _isInitialized = true;
+                    }
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Already was initialized");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
