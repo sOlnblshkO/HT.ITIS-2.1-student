0c4fbd3a805492262f47379be07b20121b1ccd0c
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2023-09-26 21:27:42 +0300

    double checking version DOESN'T WORK and idc why

--- Tests.CSharp/Homework3/SingleInitializationSingleton.cs
+++ Tests.CSharp/Homework3/SingleInitializationSingleton.cs
@@ -6,6 +6,7 @@ public class SingleInitializationSingleton
     private static readonly object Locker = new();
 
     private static volatile bool _isInitialized = false;
+    private static SingleInitializationSingleton? _instance;
 
     private SingleInitializationSingleton(int delay = DefaultDelay)
     {
@@ -16,15 +17,43 @@ public class SingleInitializationSingleton
 
     public int Delay { get; }
 
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
+    public static SingleInitializationSingleton Instance
+    {
+        get
+        {
+            if (_instance==null)
+            {
+                lock (Locker)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingleInitializationSingleton();
+                        _isInitialized = true;
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        lock (Locker)
+        {
+            _instance = null;
+            _isInitialized = false;
+        }
     }
 
     public static void Initialize(int delay)
-    {
-        throw new NotImplementedException();
+    {   
+        lock (Locker)
+        {
+            if (_instance != null)
+                throw new InvalidOperationException("The instance has already been initialized");
+
+            _instance = new SingleInitializationSingleton(delay);
+            _isInitialized = true;
+        }
     }
 }
