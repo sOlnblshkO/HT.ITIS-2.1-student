b3578f956f4427e7c4a07118d1451f0f9bc6e526
Ruzan Valeev <61345590+coldezzt@users.noreply.github.com>
2023-09-28 18:56:17 +0300

    Update WithMutex.cs
    Добавлен аттрибут [ExcludeFromCodeCoverage]

--- Homework3/Hw3.Mutex/WithMutex.cs
+++ Homework3/Hw3.Mutex/WithMutex.cs
@@ -2,6 +2,7 @@ using System.IO.MemoryMappedFiles;
 
 namespace Hw3.Mutex;
 
+[ExcludeFromCodeCoverage]
 public class WithMutex: IDisposable
 {
     private static readonly string MutexName = "Global\\MyMutex__!";
