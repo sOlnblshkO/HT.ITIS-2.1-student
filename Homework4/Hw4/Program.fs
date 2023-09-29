a025807653ae2ba6b18b21cbb36616ca154e3673
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2023-09-29 18:56:42 +0300

    Rewrote calculator to F#

--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,11 @@
-﻿raise (System.NotImplementedException())
+﻿open Hw4
+
+[<EntryPoint>]
+let main args =
+    try
+        let calcOptions = Parser.parseCalcArguments args
+        printfn "%f" (Calculator.calculate calcOptions.arg1 calcOptions.operation calcOptions.arg2)
+    with e ->
+        printfn "%s" e.Message
+
+    0
