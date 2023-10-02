6edbc86d62d365e79f4ee54b2aef9d50c6602277
irekkkarimov <irekkkarimov@mail.ru>
2023-10-02 16:25:28 +0300

    done hw4

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -11,10 +11,32 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+    if (args.Length = 3)
+    then true
+    else raise (ArgumentException("Wrong args length"))
 
 let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> ArgumentException("Wrong operation") |> raise
     
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    isArgLengthSupported(args) |> ignore
+    let operation = parseOperation(args[1])
+    let (isArg1Converted, arg1) = Double.TryParse(args[0])
+    let (isArg2Converted, arg2) = Double.TryParse(args[2])
+    
+    if (not isArg1Converted || not isArg2Converted)
+    then ArgumentException("Wrong args") |> raise
+    
+    let calcOptions = {
+        arg1 = arg1
+        arg2 = arg2
+        operation = operation
+    }
+    
+    calcOptions
+     
--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,10 @@
-﻿raise (System.NotImplementedException())
+﻿
+[<EntryPoint>]
+let main args =
+    let arg1 = args[0]
+    let operation = args[1]
+    let arg2 = args[2]
+    
+    let result = $"{arg1}{operation}{arg2}"
+    printfn "%s" result
+    0
