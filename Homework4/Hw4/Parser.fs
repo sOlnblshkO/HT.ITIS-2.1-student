9deaee65ec503ede2f030dfc239aeec12b132f5a
Coldbatt <cold-batt1486@mail.ru>
2023-10-03 22:29:03 +0300

    [Add] Homework №4

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -3,7 +3,6 @@
 open System
 open Hw4.Calculator
 
-
 type CalcOptions = {
     arg1: float
     arg2: float
@@ -11,10 +10,23 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
-
-let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    args.Length = 3
     
+let parseNumber(arg: string) =
+    let mutable result: double = 0
+    if (Double.TryParse(arg,  &result)) then result
+    else raise(ArgumentException($"Incorrect number: {arg}"))
+    
+let parseOperation (arg : string) =
+    match arg with
+        | "+" -> CalculatorOperation.Plus
+        | "-" -> CalculatorOperation.Minus   
+        | "*" -> CalculatorOperation.Multiply   
+        | "/" -> CalculatorOperation.Divide   
+        | _ -> raise(ArgumentException($"Incorrect operation - {arg}"))
+        
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    if (not(isArgLengthSupported(args))) then
+        raise(ArgumentException("Incorrect count of arguments"))
+    {arg1 = parseNumber(args[0]); arg2 = parseNumber(args[2]); operation = parseOperation(args[1])}
+        
--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,7 @@
-﻿raise (System.NotImplementedException())
+﻿open Hw4
+open Hw4.Parser
+
+let args: string[] = [|"3"; "+"; "4"|]
+let parsedArguments = Parser.parseCalcArguments(args)
+
+printfn $"%f{Calculator.calculate parsedArguments.arg1 parsedArguments.operation parsedArguments.arg2}"
