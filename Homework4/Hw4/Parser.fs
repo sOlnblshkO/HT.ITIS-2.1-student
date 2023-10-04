37136cad19b3a7bf8607be7605fab66cb5441acd
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2023-10-04 08:00:52 +0300

    hw4 is complete

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -11,10 +11,32 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+    Array.length args = 3
 
 let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus  
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> InvalidOperationException("Некорректная операция") |> raise
+
     
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    match isArgLengthSupported args with
+    | false -> ArgumentException("Некорректное количество аргументов") |> raise
+    | true ->
+        let op = parseOperation args[1]
+        match Double.TryParse args[0] with
+        | true, val1 ->
+             match Double.TryParse args[2] with
+             | true, val2 -> { arg1 = val1; arg2 = val2; operation = op }
+             | false, _ -> ArgumentException("Некорректные значения") |> raise
+        | false, _ -> ArgumentException("Некорректные значения") |> raise
+
+    
+    
+
+    
+
+    
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Домашняя работа для третьего учебного семестра (2 год обучения, 1 семестр)
 
 ![.NET](https://github.com/schwrmt/HT-ITIS.2.1-student/actions/workflows/dotnet.yml/badge.svg)
-[![codecov](https://codecov.io/gh/schwrmt/HT-ITIS.2.1-student/branch/hw3/graph/badge.svg?token=QKUVYUG8OD)](https://codecov.io/gh/schwrmt/HT-ITIS.2.1-student)
+[![codecov](https://codecov.io/gh/schwrmt/HT-ITIS.2.1-student/branch/hw4/graph/badge.svg?token=QKUVYUG8OD)](https://codecov.io/gh/schwrmt/HT-ITIS.2.1-student)
 
 
 ## Как устроены Actions
