9b8679c2856e57abc35f5e251dfcd51a4740876d
Akhmed Mirsaitov <bignumbergames@gmail.com>
2023-10-04 17:48:32 +0300

    HW-4 Реализовал parser.fs

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -10,11 +10,21 @@ type CalcOptions = {
     operation: CalculatorOperation
 }
 
-let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+let isArgLengthSupported (args : string[]) : bool =
+    args.Length = 3
 
 let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> raise (new ArgumentException($"Invalid argument: {arg}"))
     
-let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+let parseCalcArguments(args : string[]) : CalcOptions =
+    if not (isArgLengthSupported args) then raise (new ArgumentException("Invalid argument count"))
+    let isOk1, val1 = Double.TryParse args[0]
+    if not isOk1 then raise (new ArgumentException("First argument invalid"))
+    let isOk2, val2 = Double.TryParse args[2]
+    if not isOk2 then raise (new ArgumentException("Second argument invalid"))
+    {CalcOptions.arg1 = val1; CalcOptions.arg2 = val2; CalcOptions.operation = parseOperation args[1]}
