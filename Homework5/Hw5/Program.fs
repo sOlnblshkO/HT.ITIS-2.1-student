cfae461ab2bf142b5c3515a4cc57b7d196b8d949
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2023-10-08 22:03:17 +0300

    Added HW5

--- Homework5/Hw5/Program.fs
+++ Homework5/Hw5/Program.fs
@@ -1,3 +1,17 @@
-﻿open System
+﻿open Hw5
 
-(NotImplementedException() |> raise)       
+[<EntryPoint>]
+let main args =
+    match Parser.parseCalcArguments args with
+    | Ok (arg1, operation, arg2) -> Calculator.calculate arg1 operation arg2 |> printfn "%f"
+    | Error errorValue ->
+        match errorValue with
+        | Message.DivideByZero -> "Division by zero"
+        | Message.WrongArgFormat -> "Parsing error"
+        | Message.WrongArgLength -> "Wrong number of arguments"
+        | Message.WrongArgFormatOperation -> "Operation not supported"
+        | _ -> "Unknown error"
+        |> printfn "Error: %s"
+
+
+    0
