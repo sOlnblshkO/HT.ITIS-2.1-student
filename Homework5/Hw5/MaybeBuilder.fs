a2301ce9d1bd03a86f50aca2ea8ef69091ab4360
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-10-09 20:10:26 +0300

    "values parsed correctly" test failed

--- Homework5/Hw5/MaybeBuilder.fs
+++ Homework5/Hw5/MaybeBuilder.fs
@@ -4,7 +4,8 @@ open System
 
 type MaybeBuilder() =
     member builder.Bind(a, f): Result<'e,'d> =
-        (NotImplementedException() |> raise)
-    member builder.Return x: Result<'a,'b> =
-        (NotImplementedException() |> raise)
+        match a with
+        | Ok x -> f x
+        | Error e -> Error e
+    member builder.Return x: Result<'a,'b> = Ok x
 let maybe = MaybeBuilder()
