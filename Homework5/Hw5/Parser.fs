286385cbf5f8ee80ca0316a7912dbad33cd5232d
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2023-10-11 01:50:26 +0300

    hw5 initial commit

--- Homework5/Hw5/Parser.fs
+++ Homework5/Hw5/Parser.fs
@@ -2,20 +2,50 @@
 
 open System
 open Hw5.Calculator
+open Hw5.MaybeBuilder
 
 let isArgLengthSupported (args:string[]): Result<'a,'b> =
-    (NotImplementedException() |> raise)
+    let tupleArgs = (args[0],args[1],args[2])
+    match (args.Length = 3) with
+    |true -> Ok tupleArgs  
+    |false -> Error Message.WrongArgLength
     
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isOperationSupported (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)
+    match operation with
+    | Calculator.plus -> Ok (arg1, CalculatorOperation.Plus, arg2)
+    | Calculator.minus -> Ok (arg1, CalculatorOperation.Minus, arg2)
+    | Calculator.multiply -> Ok (arg1, CalculatorOperation.Multiply, arg2)
+    | Calculator.divide -> Ok (arg1, CalculatorOperation.Divide, arg2)
+    | _ -> Error Message.WrongArgFormatOperation
 
+
+let stringDoubleParser (arg : string) =
+    match Double.TryParse(arg) with
+    | true, value -> Ok value
+    | _ -> Error Message.WrongArgFormat
 let parseArgs (args: string[]): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)    
+    maybe{
+        let! tupleArgs = isArgLengthSupported args
+        let! arg1 = stringDoubleParser args[0]
+        let! arg2 =  stringDoubleParser args[2]
+        let! _, operation, _ = isOperationSupported tupleArgs
+        
+        return arg1,operation,arg2
+    }
 
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isDividingByZero (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)       
+    match (operation) with
+    | CalculatorOperation.Divide ->
+        match arg2 with
+        |0.0 -> Error Message.DivideByZero
+        | _ -> Ok (arg1, operation, arg2)
+    | _ -> Ok (arg1, operation, arg2)
     
 let parseCalcArguments (args: string[]): Result<'a, 'b> =
-    (NotImplementedException() |> raise)    
+    maybe {
+        let! tupleArgs = parseArgs args
+        let! finalArgs = isDividingByZero tupleArgs
+        return finalArgs
+    }
--- Homework5/Hw5/Program.fs
+++ Homework5/Hw5/Program.fs
@@ -1,3 +1,19 @@
 ﻿open System
+open Hw5
+open Microsoft.FSharp.Core
 
-(NotImplementedException() |> raise)       
+[<EntryPoint>]
+let main args =
+    match Parser.parseCalcArguments args with
+    | Ok (arg1, operation, arg2) -> Calculator.calculate arg1 operation arg2 |> printfn "%f"
+    | Error errorMsg ->
+        match errorMsg with
+        | Message.WrongArgLength -> "Length should be 3"
+        | Message.WrongArgFormat -> "arg parse error"
+        | Message.WrongArgFormatOperation -> "operation is not supported"
+        | Message.DivideByZero -> "division by 0"
+        | _ -> "unknown error"
+        |> printfn "error occured: %s"
+
+
+    0
