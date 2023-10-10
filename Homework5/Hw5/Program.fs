46bfa85092dd6a4b374eaa2094099b4268c0f625
Akhmed Mirsaitov <bignumbergames@gmail.com>
2023-10-10 23:01:41 +0300

    HW-5 Реализовал Program.fs

--- Homework5/Hw5/Program.fs
+++ Homework5/Hw5/Program.fs
@@ -6,5 +6,10 @@ open Hw5.Calculator
 let main argv =
     printfn "%A" argv
     let options = parseCalcArguments argv
-    
+    let result = match options with
+    | Ok (arg1, operation, arg2) ->
+        calculate arg1 operation arg2
+    | Error err -> Double.NaN
+
+    printf "result: %f" result
     0
