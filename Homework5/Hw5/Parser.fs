124b773103160dcf78e12a4cec2f00466ddbcd86
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2023-10-12 20:30:30 +0300

    Hw5

--- Homework5/Hw5/Parser.fs
+++ Homework5/Hw5/Parser.fs
@@ -2,20 +2,44 @@
 
 open System
 open Hw5.Calculator
+open System.Globalization
 
 let isArgLengthSupported (args:string[]): Result<'a,'b> =
-    (NotImplementedException() |> raise)
+    match Array.length args = 3 with
+    | true -> Ok args
+    | false -> Error Message.WrongArgLength
     
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isOperationSupported (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)
+    match operation with
+    | "+" -> Ok (arg1, CalculatorOperation.Plus, arg2)
+    | "-" -> Ok (arg1, CalculatorOperation.Minus, arg2)
+    | "*" -> Ok (arg1, CalculatorOperation.Multiply, arg2)
+    | "/" -> Ok (arg1, CalculatorOperation.Divide, arg2)
+    | _ -> Error Message.WrongArgFormatOperation
 
-let parseArgs (args: string[]): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)    
+let parseArgs (args: string[]): Result<('a * string * 'b), Message> =
+    match Double.TryParse(args[0], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture) with
+    | true, val1 ->
+         match Double.TryParse(args[2], NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture) with
+         | true, val2 -> Ok (val1, args[1], val2)
+         | false, _ -> Error Message.WrongArgFormat
+    | false, _ -> Error Message.WrongArgFormat
 
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isDividingByZero (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
-    (NotImplementedException() |> raise)       
+    match operation with
+    | CalculatorOperation.Divide ->
+        match arg2 with
+        | 0.0 -> Error Message.DivideByZero
+        | _ -> Ok (arg1, operation, arg2)
+    | _ -> Ok (arg1, operation, arg2)
     
 let parseCalcArguments (args: string[]): Result<'a, 'b> =
-    (NotImplementedException() |> raise)    
+    MaybeBuilder.maybe {
+        let! args = isArgLengthSupported args
+        let! args = parseArgs args
+        let! args = isOperationSupported args
+        let! args = isDividingByZero args
+        return args
+    }
--- Homework5/Hw5/Program.fs
+++ Homework5/Hw5/Program.fs
@@ -1,3 +1,13 @@
 ﻿open System
+open Hw5
 
-(NotImplementedException() |> raise)       
+[<EntryPoint>]
+
+while true do
+    let input = Console.ReadLine()
+    let strings = input.Split ' '
+    let args = Parser.parseCalcArguments strings
+
+    match args with
+    | Error e -> Console.WriteLine(e)
+    | Ok (val1, op, val2) -> Console.WriteLine(Calculator.calculate val1 op val2)
