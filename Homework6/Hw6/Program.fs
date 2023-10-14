f25a9d7d1edb899963378f9aafa00ad4ce888fb5
IlyaKlim23 <klimkin_ilya2004@mail.ru>
2023-10-14 23:56:02 +0300

    Add hw6

--- Homework6/Hw6/Program.fs
+++ Homework6/Hw6/Program.fs
@@ -1,6 +1,5 @@
 module Hw6.App
 
-open System
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
@@ -8,10 +7,47 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 
-let calculatorHandler: HttpHandler =
-    fun next ctx ->
-        let result: Result<string, string> = raise(NotImplementedException())
+[<CLIMutable>]
+type calcArgs =
+    { value1: double
+      operation: string
+      value2: double }
 
+[<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
+let calculate (value1: double, operation: string, value2: double) =
+    match operation with
+    | "+" -> Ok $"{value1 + value2}"
+    | "-" -> Ok $"{value1 - value2}"
+    | "*" -> Ok $"{value1 * value2}"
+    | "/" -> if value2 <> 0.0 then Ok $"{value1 / value2}"
+             else Ok "DivideByZero"
+    | _ -> Error $"Could not parse value '{operation}'"
+
+type MaybeBuilder() =
+    member this.Bind(a, f): Result<'e,'d> =
+        match a with
+        | Ok ok -> f ok
+        | Error err -> Error err
+    member this.Return x: Result<'a,'b> =
+        Ok x
+        
+let maybe = MaybeBuilder()
+
+let parseOperation operation =
+    match operation with
+    | "Plus" -> "+"
+    | "Minus" -> "-"
+    | "Multiply" -> "*"
+    | "Divide" -> "/"
+    | _ -> operation
+    
+let calculatorHandler : HttpHandler =
+    fun next ctx ->
+        let result = maybe {
+            let! args = ctx.TryBindQueryString<calcArgs>()
+            let! parsed = calculate (args.value1, parseOperation args.operation, args.value2)
+            return parsed
+        }
         match result with
         | Ok ok -> (setStatusCode 200 >=> text (ok.ToString())) next ctx
         | Error error -> (setStatusCode 400 >=> text error) next ctx
@@ -19,6 +55,7 @@ let calculatorHandler: HttpHandler =
 let webApp =
     choose [
         GET >=> choose [
+             route "/calculate" >=> calculatorHandler
              route "/" >=> text "Use //calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>"
         ]
         setStatusCode 404 >=> text "Not Found" 
