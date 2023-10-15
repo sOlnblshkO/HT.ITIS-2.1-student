4d9865054ec7e1563129aef4b40ea13bcd4b1220
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-10-15 15:34:14 +0300

    added Client.fs

---
+++ Homework6/Hw6.Client/Client.fs
@@ -0,0 +1,31 @@
+﻿open System
+open System.Net.Http
+
+let parseOperation operation =
+    match operation with
+    | "+" -> "Plus"
+    | "-" -> "Minus"
+    | "*" -> "Multiply"
+    | "/" -> "Divide"
+    | _ -> operation
+
+
+let sendRequestAsync(client : HttpClient) (url : string) =
+    async {
+        let! response = Async.AwaitTask (client.GetAsync url)
+        let! res = Async.AwaitTask (response.Content.ReadAsStringAsync())
+        return res
+    }
+    
+    
+[<EntryPoint>]
+let main args =
+    let input = Console.ReadLine()
+    use handler = new HttpClientHandler()
+    use client = new HttpClient(handler)
+    let args = input.Split(" ")
+    if args.Length = 3 then
+        let url = $"https://localhost:56752/calculate?value1={args[0]}&operation={parseOperation args[1]}&value2={args[2]}"
+        let result = Async.RunSynchronously(sendRequestAsync client url)
+        printf $"{result}" 
+    0
