43dce6c5914d57aaf143f875c0993c80dce0c8b3
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-10-15 14:36:33 +0300

    done HW6

--- Homework6/Hw6/Program.fs
+++ Homework6/Hw6/Program.fs
@@ -1,6 +1,7 @@
 module Hw6.App
 
 open System
+open System.Globalization
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
@@ -8,22 +9,49 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 
+let parseArgument (arg: string) =
+    match Double.TryParse(arg, NumberStyles.Float, CultureInfo.InvariantCulture) with
+    | (true, num) -> Ok num
+    | _ -> Error $"Could not parse value '{arg}'"
+    
+let Calculate (value1: string, operation: string, value2: string): Result<string, string> =
+   
+    let val1 = parseArgument value1
+    let val2 = parseArgument value2
+    match val1 with
+        | Ok val1 ->  match val2 with
+                      | Ok val2 -> match operation with
+                                     | "Plus" -> Ok ((val1+val2).ToString())
+                                     | "Minus" -> Ok ((val1-val2).ToString())
+                                     | "Multiply" -> Ok ((val1*val2).ToString())
+                                     | "Divide" -> match val2 with
+                                                   | 0.0 -> Error "DivideByZero"
+                                                   | _ -> Ok ((val1/val2).ToString())
+                                     | _ -> Error $"Could not parse value '{operation}'"
+                      | Error errorMessage -> Error errorMessage
+        | Error errorMessage -> Error errorMessage
+    
+    
 let calculatorHandler: HttpHandler =
     fun next ctx ->
-        let result: Result<string, string> = raise(NotImplementedException())
-
+        let result: Result<string, string> = Calculate (ctx.Request.Query.Item "value1", ctx.Request.Query.Item "operation", ctx.Request.Query.Item "value2")
         match result with
         | Ok ok -> (setStatusCode 200 >=> text (ok.ToString())) next ctx
-        | Error error -> (setStatusCode 400 >=> text error) next ctx
-
+        | Error errorMessage -> (setStatusCode 400 >=> text errorMessage) next ctx
+        
 let webApp =
     choose [
         GET >=> choose [
-             route "/" >=> text "Use //calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>"
-        ]
+             route "/" >=> text "/calculate?value1=<VAL1>&operation=<OPERATION>&value2=<VAL2>"
+             route "/hello" >=> text "Hello"
+             route "/calculate" >=> calculatorHandler 
+                
+                
+            ]
         setStatusCode 404 >=> text "Not Found" 
     ]
     
+
 type Startup() =
     member _.ConfigureServices (services : IServiceCollection) =
         services.AddGiraffe() |> ignore
