d96675369ec292c10a395141095c397369337e47
Ilnar Gazizov <IlnIGazizov@stud.kpfu.ru>
2023-10-17 17:37:19 +0300

    Extracted functions into a separate file and added new test
    Mostly for testing code coverage, as *Program.fs files are ignored by default.

--- Homework6/Hw6/Program.fs
+++ Homework6/Hw6/Program.fs
@@ -1,54 +1,17 @@
 module Hw6.App
 
-open System
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 
-let (>>=) a f = Result.bind f a
-
-let parseValue (value: string) =
-    let success, parsed = Double.TryParse value
-
-    match success with
-    | true -> Ok parsed
-    | _ -> Error $"Could not parse value '{value}'"
-
-let parseOperation (value1: double) (value2: double) (operation: string) =
-    match operation with
-    | "Plus" -> Ok(string (value1 + value2))
-    | "Minus" -> Ok(string (value1 - value2))
-    | "Multiply" -> Ok(string (value1 * value2))
-    | "Divide" when value2 = 0 -> Ok "DivideByZero" // Honestly an error would be more fitting
-    | "Divide" -> Ok(string (value1 / value2))
-    | _ -> Error $"Could not parse value '{operation}'"
-
-let getFromQuery (key: string) (ctx: HttpContext) =
-    let strings = ctx.Request.Query[key]
-
-    match strings.Count with
-    | 0 -> Error $"'{key}' not present in a query"
-    | c -> Ok strings[c - 1]
-
-let collectValue1 ctx =
-    ctx |> getFromQuery "value1" >>= parseValue
-
-let collectValue2 ctx value1 =
-    match ctx |> getFromQuery "value2" >>= parseValue with
-    | Ok value2 -> Ok(value1, value2)
-    | Error error -> Error error
-
-let collectOperation ctx (value1, value2) =
-    ctx |> getFromQuery "operation" >>= parseOperation value1 value2
-
+open Hw6.WebCalculator
 
 let calculatorHandler: HttpHandler =
     fun next ctx ->
-        let result = collectValue1 ctx >>= collectValue2 ctx >>= collectOperation ctx
+        let result = calculate ctx
 
         match result with
         | Ok ok -> (setStatusCode 200 >=> text (ok.ToString())) next ctx
