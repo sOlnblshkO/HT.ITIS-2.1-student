43b11a4326986be91ad965daaffef35df3078f3b
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2023-10-29 14:27:37 +0300

    Hw7 done initial commit

---
+++ Homework7/Hw7/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Hw7.Models.ForTests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hw7.Controllers;
+
+public class TestController : Controller
+{
+    [HttpGet]
+    public IActionResult TestModel()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult TestModel(TestModel profile)
+    {
+        return View(profile);
+    }
+}
--- Homework7/Hw7/MyHtmlServices/HtmlHelperExtensions.cs
+++ Homework7/Hw7/MyHtmlServices/HtmlHelperExtensions.cs
@@ -1,3 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -7,6 +11,84 @@ public static class HtmlHelperExtensions
 {
     public static IHtmlContent MyEditorForModel(this IHtmlHelper helper)
     {
-        throw new NotImplementedException();
+        var entity = helper.ViewData.Model;
+        var properties = helper.ViewData.ModelMetadata.ModelType.GetProperties();
+
+        return new HtmlString(GetHtml(entity, properties));
+    }
+
+    private static String GetHtml(object? entity, PropertyInfo[] propertyInfos)
+    {
+        var htmlString = new StringBuilder();
+        foreach (var propertyInfo in propertyInfos)
+        {
+            var display = HandleDisplay(propertyInfo);
+            htmlString.AppendLine("<div>");
+            htmlString.AppendLine($"<label for=\"{propertyInfo.Name}\">{display}</label><br>");
+            htmlString.AppendLine(propertyInfo.PropertyType.IsEnum
+                ? ParseEnum(propertyInfo)
+                : ParseNotEnum(propertyInfo));
+            var validationResult = ValidateField(entity, propertyInfo);
+            if (validationResult != null)
+            {
+                htmlString.AppendLine($"<span>{validationResult}</span>");
+            }
+
+            htmlString.AppendLine("</div><br>");
+        }
+        return htmlString.ToString();
+    }
+
+    private static String ParseEnum(PropertyInfo propertyInfo)
+    {
+        var sb = new StringBuilder();
+        
+        sb.AppendLine($"<select name=\"{propertyInfo.Name}\" required>");
+        foreach (var value in Enum.GetValues(propertyInfo.PropertyType))
+        {
+            sb.AppendLine($"<option value=\"{value}\">{value}</option>");
+        }
+
+        sb.AppendLine("</select>");
+        
+        return sb.ToString();
+    }
+
+    private static String ParseNotEnum(PropertyInfo propertyInfo)
+    {
+        
+        string type = propertyInfo.PropertyType == typeof(string) ? "text" : "number";
+        return $"<input type=\"{type}\" name=\"{propertyInfo.Name}\" id=\"{propertyInfo.Name}\"/>";
+    }
+
+    private static String HandleDisplay(PropertyInfo propertyInfo)
+    {
+        var displayAttribute = propertyInfo.GetCustomAttribute<DisplayAttribute>();
+        if (displayAttribute != null)
+        {
+            return displayAttribute.Name!;
+        }
+        return Regex.Replace(propertyInfo.Name, "([A-Z])", " $1", RegexOptions.Compiled).Trim();
+    }
+
+    private static string? ValidateField(object? entity, PropertyInfo propertyInfo)
+    {
+        string? resp = null;
+
+        if (entity != null)
+        {
+            var validationAttributes = propertyInfo.GetCustomAttributes<ValidationAttribute>();
+            foreach (var attribute in validationAttributes)
+            {
+                if (!attribute.IsValid(propertyInfo.GetValue(entity)))
+                {
+                    resp = attribute.ErrorMessage;
+                    break;
+                }
+                
+            }
+        }
+        
+        return resp;
     }
 } 
