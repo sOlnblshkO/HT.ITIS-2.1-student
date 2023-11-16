ce9ec8a9a45eb5169f0d6409778b1071bf62708c
Resqdeeo <vlazer03@mail.ru>
2023-11-16 20:16:45 +0300

    done hw9

---
+++ Homework9/Hw9/ExpressionHelper/ExpressionParser.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Hw9.ExpressionHelper;
+
+public class ExpressionParser
+{
+    private readonly Dictionary<string, int> _operationPriority = new()
+    {
+        { "(", 0 },
+        { "+", 1 },
+        { "-", 1 },
+        { "*", 2 },
+        { "/", 2 },
+        { "~", 4 }
+    };
+
+    public string ParseToPolishNotation(string[] expressionInput)
+    {
+        var expression = expressionInput
+            .Select(i => i.Replace(" ", ""))
+            .Where(i => i != "")
+            .ToArray();
+        var postfix = new StringBuilder();
+        var operations = new Stack<string>();
+        var isPreviousOpenParenthesis = false;
+        var numberPattern = new Regex(@"^\d+");
+
+        foreach (var t in expression)
+        {
+            var element = t;
+
+            if (numberPattern.IsMatch(element))
+            {
+                postfix.Append(element);
+                postfix.Append(' ');
+                isPreviousOpenParenthesis = false;
+                continue;
+            }
+
+            switch (element)
+            {
+                case "(":
+                {
+                    operations.Push(element);
+                    isPreviousOpenParenthesis = true;
+                    continue;
+                }
+                case ")":
+                {
+                    while (operations.Peek() != "(")
+                    {
+                        postfix.Append(operations.Pop());
+                        postfix.Append(' ');
+                    }
+
+                    operations.Pop();
+                    isPreviousOpenParenthesis = false;
+                    continue;
+                }
+            }
+
+            if (element == "-" && isPreviousOpenParenthesis)
+                element = "~";
+            isPreviousOpenParenthesis = false;
+
+            while (operations.Any() && _operationPriority[operations.Peek()] >= _operationPriority[element])
+            {
+                postfix.Append(operations.Pop());
+                postfix.Append(' ');
+            }
+
+            operations.Push(element);
+        }
+
+        foreach (var operation in operations)
+        {
+            postfix.Append(operation);
+            postfix.Append(' ');
+        }
+
+        return postfix.ToString();
+    }
+}
---
+++ Homework9/Hw9/ExpressionHelper/ExpressionTreeConverter.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+
+namespace Hw9.ExpressionHelper;
+
+public class ExpressionTreeConverter
+{
+    public static Expression ConvertToExpressionTree(string expressionString)
+    {
+        var expressions = expressionString.Split(" ")
+            .Where(i => i != "")
+            .ToArray();
+        var expressionStack = new Stack<Expression>();
+
+        foreach (var expressionElement in expressions)
+        {
+            if (double.TryParse(expressionElement, out var operand))
+            {
+                expressionStack.Push(Expression.Constant(operand));
+            }
+            else
+            {
+                if (expressionElement == "~")
+                {
+                    var value = expressionStack.Pop();
+                    expressionStack.Push(Expression.Negate(value));
+                }
+                else
+                {
+                    var right = expressionStack.Pop();
+                    var left = expressionStack.Pop();
+
+                    var expression = expressionElement switch
+                    {
+                        "+" => Expression.Add(left, right),
+                        "-" => Expression.Subtract(left, right),
+                        "*" => Expression.Multiply(left, right),
+                        _ => Expression.Divide(left, right)
+                    };
+
+                    expressionStack.Push(expression);
+                }
+            }
+        }
+
+        return expressionStack.Pop();
+    }
+}
