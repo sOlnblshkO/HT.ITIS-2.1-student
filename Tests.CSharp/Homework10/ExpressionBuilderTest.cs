ba03a633e3376a906a2e02f0caa1bd89a474ec97
Nigmatyanov Ilnur <ilnurnigmatanov97@gmail.com>
2023-11-20 03:04:42 +0300

    i wantsleep

---
+++ Tests.CSharp/Homework10/ExpressionBuilderTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Hw9.Services.MathCalculator;
+using Tests.RunLogic.Attributes;
+
+namespace Tests.CSharp.Homework10;
+
+public class ExpressionBuilderTest
+{
+    [Homework(Homeworks.HomeWork9)]
+    public void VisitBinaryDivision()
+    {
+        Assert.Throws<Exception>(() => ExpressionBuilder.ConvertToExpression("1 3 ^"));
+    }
+}
---
+++ Tests.CSharp/Homework10/ExpressionTreeVisitorTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using Hw9.Services.MathCalculator;
+using Tests.RunLogic.Attributes;
+
+namespace Tests.CSharp.Homework10;
+
+public class ExpressionTreeVisitorTest
+{
+    [Homework(Homeworks.HomeWork9)]
+    public void VisitBinaryDivision()
+    {
+        var constant1 = Expression.Constant(10.0, typeof(double));
+        var constant2 = Expression.Constant(2.0, typeof(double));
+        
+        var division = Expression.Divide(constant1, constant2);
+        
+        ExpressionVisitor expressionVisitor = new ExpressionTreeVisitorImpl();
+        var result = expressionVisitor.Visit(division);
+        
+        var divide = Expression.Lambda<Func<double>>(result).Compile().Invoke();
+        Assert.Equal(5.0,divide);
+    }
+    [Homework(Homeworks.HomeWork9)]
+    public void VisitBinaryDivisionByZer0()
+    {
+        var constant1 = Expression.Constant(10.0, typeof(double));
+        var constant2 = Expression.Constant(0.0, typeof(double));
+        
+        var division = Expression.Divide(constant1, constant2);
+        
+        ExpressionVisitor expressionVisitor = new ExpressionTreeVisitorImpl();
+
+        Assert.Throws<Exception>(() => expressionVisitor.Visit(division));
+    }
+    [Homework(Homeworks.HomeWork9)]
+    public void VisitBinaryUnknownExpression()
+    {
+        var constant1 = Expression.Constant(10.0, typeof(double));
+        var constant2 = Expression.Constant(3.0, typeof(double));
+        
+        var division = Expression.Power(constant1, constant2);
+        
+        ExpressionVisitor expressionVisitor = new ExpressionTreeVisitorImpl();
+
+        Assert.Throws<Exception>(() => expressionVisitor.Visit(division));
+    }
+}
