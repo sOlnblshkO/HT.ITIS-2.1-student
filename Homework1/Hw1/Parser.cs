b62d6d52260f2b2c4c719e73653feecdd001b192
1chess <1chessmic@gmail.com>
2023-11-22 17:44:04 +0300

    hw01

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,13 +7,28 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args))
+            throw new ArgumentException($"Need 3 arguments, was given {args.Length}");
+
+        if (!(double.TryParse(args[0], out _) && double.TryParse(args[2], out _)))
+            throw new ArgumentException($"Wrong arguments, expected numbers, was given {args[0]} and {args[2]}");
+
+        val1 = double.Parse(args[0]);
+        operation = ParseOperation(args[1]);
+        val2 = double.Parse(args[2]);
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        return arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => throw new InvalidOperationException($"Expected operation, was given {arg}")
+        };
     }
 }
