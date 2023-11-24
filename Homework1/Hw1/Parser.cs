19464e6b1d3f8e3066b23621a014453bc3445af9
1chess <1chessmic@gmail.com>
2023-11-24 20:12:55 +0300

    Исправлены замечания

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -10,12 +10,10 @@ public static class Parser
         if (!IsArgLengthSupported(args))
             throw new ArgumentException($"Need 3 arguments, was given {args.Length}");
 
-        if (!(double.TryParse(args[0], out _) && double.TryParse(args[2], out _)))
+        if (!(double.TryParse(args[0], out val1) && double.TryParse(args[2], out val2)))
             throw new ArgumentException($"Wrong arguments, expected numbers, was given {args[0]} and {args[2]}");
 
-        val1 = double.Parse(args[0]);
         operation = ParseOperation(args[1]);
-        val2 = double.Parse(args[2]);
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
@@ -28,7 +26,7 @@ public static class Parser
             "-" => CalculatorOperation.Minus,
             "*" => CalculatorOperation.Multiply,
             "/" => CalculatorOperation.Divide,
-            _ => throw new InvalidOperationException($"Expected operation, was given {arg}")
+            _ => throw new InvalidOperationException($"Expected operation(+, -, *, /), was given {arg}")
         };
     }
 }
