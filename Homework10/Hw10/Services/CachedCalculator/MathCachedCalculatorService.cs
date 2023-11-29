4a6452ab51feba6973bce633e1c07249c72b29ca
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2023-11-29 10:18:24 +0300

    перезалив 10 домашки

--- Homework10/Hw10/Services/CachedCalculator/MathCachedCalculatorService.cs
+++ Homework10/Hw10/Services/CachedCalculator/MathCachedCalculatorService.cs
@@ -6,17 +6,46 @@ namespace Hw10.Services.CachedCalculator;
 
 public class MathCachedCalculatorService : IMathCalculatorService
 {
-	private readonly ApplicationContext _dbContext;
-	private readonly IMathCalculatorService _simpleCalculator;
+    private readonly ApplicationContext _dbContext;
+    private readonly IMathCalculatorService _simpleCalculator;
 
-	public MathCachedCalculatorService(ApplicationContext dbContext, IMathCalculatorService simpleCalculator)
-	{
-		_dbContext = dbContext;
-		_simpleCalculator = simpleCalculator;
-	}
+    public MathCachedCalculatorService(ApplicationContext dbContext, IMathCalculatorService simpleCalculator)
+    {
+        _dbContext = dbContext;
+        _simpleCalculator = simpleCalculator;
+    }
 
-	public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
-	{
-		throw new NotImplementedException();
-	}
+    public async Task<CalculationMathExpressionResultDto> CalculateMathExpressionAsync(string? expression)
+    {
+        if (!string.IsNullOrEmpty(expression))
+        {
+            var solvingExpressions = _dbContext.SolvingExpressions;
+            var solvingExpressionsWhere = solvingExpressions.Where(i => i.Expression == expression.Trim());
+            if (solvingExpressionsWhere.Any())
+            {
+                var expressionResult = solvingExpressionsWhere.First().Result;
+				
+                await Task.Delay(500);
+				
+                return new CalculationMathExpressionResultDto(expressionResult);
+            }
+        }
+		
+		
+        var result = await _simpleCalculator.CalculateMathExpressionAsync(expression);
+
+        if (!result.IsSuccess) return new CalculationMathExpressionResultDto(result.ErrorMessage);
+        var solvingExpression = new SolvingExpression
+        {
+            SolvingExpressionId = 0,
+            Expression = expression!.Trim(),
+            Result = result.Result
+        };
+			
+        _dbContext.SolvingExpressions.Add(solvingExpression);
+        await _dbContext.SaveChangesAsync();
+			
+        return new CalculationMathExpressionResultDto(result.Result);
+
+    }
 }
