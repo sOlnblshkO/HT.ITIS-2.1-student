e1eb26b7b562eba1eeb3473838f24c264c6c4307
ilyaklim23 <klimkin_ilya2004@mail.ru>
2023-12-07 22:50:11 +0300

    hw12

--- Homework12/Hw12/TestApplicationFactoryFSharp.cs
+++ Homework12/Hw12/TestApplicationFactoryFSharp.cs
@@ -1,8 +1,8 @@
+using Hw6;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Hw12;
 
-public class TestApplicationFactoryFSharp : WebApplicationFactory<TestApplicationFactoryFSharp>
-    // TODO: replace generic argument with the right one
+public class TestApplicationFactoryFSharp : WebApplicationFactory<App.Startup>
 {
 }
--- Tests.RunLogic/TestConfig.cs
+++ Tests.RunLogic/TestConfig.cs
@@ -1,3 +1,3 @@
 using Tests.RunLogic.Attributes;
 
-[assembly: HomeworkProgress(Homeworks.HomeWork11)]
+[assembly: HomeworkProgress(Homeworks.HomeWork12)]
